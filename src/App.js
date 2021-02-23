5e074010a8bb89158fe62856cda98736236fb9a0
Bob <rstauss@burlingtoncodeacademy.com>
2021-02-23 10:25:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: "Submit the Form to see this text change",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.value}</h2>
+        <form
+          onSubmit={(evt) => {
+            evt.preventDefault();
+            this.setState(document.getElementById("user-in").value);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <input type="text" id="user-in" />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
